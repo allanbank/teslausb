e3249d0485bbca2c40c418da998f01277bc03a7f
Robert J. Moore <Robert.J.Moore@allanbank.com>
2019-11-26 21:42:14 -0500

    Have the snapshot generation wait until after the car dumping files.

--- run/make_snapshot.sh
+++ run/make_snapshot.sh
@@ -22,6 +22,60 @@ then
   fi
 fi
 
+
+function get_current_io_on() {
+  awk '{print $7}' "/sys/block/${1}/stat"
+}
+function wait_for_post_io_surge() {
+
+  # Determined experimentally:
+  # 1 second increase in the written blocks of the device during the camera dump is 40k, 70K, 90K blocks.
+  # Normal background writes are on the order if 1-2K blocks.
+  readonly SURGE_WRITE_SECTORS=10000
+
+  local file="${1}"
+  readonly dev_with_part=$( df "${1}" | awk 'NR == 2 {print $1}' )
+  # Remove the partition from the device name.
+  local dev="${dev_with_part%p[0-9]}"
+  local dev_name="${dev##*/}"
+
+  # Make sure the module is loaded before waiting for a surge.
+  if lsmod | grep -q  g_mass_storage >> /dev/null 2>&1
+  then
+    local last_io
+    local io
+    local delta
+    local spike_start=-1
+    log "Waiting up to 2 minutes for after an I/O surge on ${dev_name} before making the snapshot."
+    io=$( get_current_io_on "${dev_name}" )
+    for i in {1..120}
+    do
+      last_io="${io}"
+
+      sleep 1
+
+      io=$( get_current_io_on "${dev_name}" )
+      delta=$(( io - last_io ))
+
+      if [ "${SURGE_WRITE_SECTORS}" -lt "${delta}"  ]
+      then
+        # We are in an I/O spike. Mark the start.
+        if [ "${spike_start}" -lt "0" ]
+        then
+          spike_start="${i}"
+        fi
+      elif [ "${spike_start}" -gt "0" ] && [ "${delta}" -le "1" ]
+      then
+        # Spike has finushed.
+        local duration=$(( i - spike_start ))
+        log "Waited ${spike_start}s fo a spike in I/O on ${dev_name} that started ${duration}s ago (and just stopped). Taking snapshot."
+        return
+      fi
+    done
+    log "Did not see an I/O surge. Taking snapshot anyway."
+  fi
+}
+
 function copy_file_to {
   local group="${1}"
   local file="${2}"
@@ -65,10 +119,10 @@ function copy_files_for_snapshot {
   do
     if [ -d "${mntpoint}/TeslaCam/${group}" ] ; then
       log "Copying files from ${mntpoint}/TeslaCam/${group}..."
-      while IFS= read -r -d '' file
+      while IFS= read -r file
       do
         copy_file_to "${group}" "${file}"
-      done < <( find "${mntpoint}/TeslaCam/${group}" -type f -print0 )
+      done < <( find "${mntpoint}/TeslaCam/${group}" -type f -print )
     else
       log "${mntpoint}/TeslaCam/${group} does not exist. Skipping copy."
     fi
@@ -128,13 +182,13 @@ function snapshot {
   freespace=$(eval $(stat --file-system --format="echo \$((%f*%S))" /backingfiles/cam_disk.bin))
   # shellcheck disable=SC2046
   fssize=$(eval $(stat --file-system --format="echo \$((%b*%S))" /backingfiles/cam_disk.bin))
-  tenpercentminfree=$(( fssize/ 10 ))
+  tenpercentminfree=$(( fssize / 10 ))
   wanted=$imgsize
 
   if [ "$imgsize" -lt $tenpercentminfree ]
   then
     # Free the larger (10%) of the disk.
-    wanted=tenpercentminfree
+    wanted="$tenpercentminfree"
   fi
   if [ "$freespace" -lt $wanted ]
   then
@@ -155,6 +209,10 @@ function snapshot {
   local snapmnt="$snapdir/mnt"
   local name="$snapdir/snap.bin"
   rm -rf "$snapdir"
+
+  # Wait for just after an I/O surge from the car dumping files.
+  wait_for_post_io_surge /backingfiles/cam_disk.bin
+
   log "Taking snapshot of cam disk: $name"
   /root/bin/mount_snapshot.sh /backingfiles/cam_disk.bin "$name" "$snapmnt"
   log "Took snapshot"
